250b7e8a205ec655eec250a29e0c51b063f93862
Voloshyn Andriy bc2024-3 <ANDRII.VOLOSHYN@lnu.edu.ua>
2024-10-14 18:04:50 +0300

    vers 2.0

---
+++ main.js
@@ -0,0 +1,53 @@
+const { program } = require('commander');
+const fs = require('fs');
+
+program
+  .requiredOption('-i, --input <path>, the path of the json file for reading')
+  .option('-o, --output <path>, the path of file with output')
+  .option("-d, --display, show the result ")
+
+  .configureOutput({
+    outputError: (str, write) => {
+        // Перевіряємо стандартні повідомлення про відсутні обов'язкові параметри
+        if (str.includes("-i")) {
+          console.error("Please specify input file");
+        } else {
+          write(str);  // Якщо це інша помилка, просто виводимо її
+        }
+    }
+  });
+
+program.parse(process.argv);
+const options = program.opts();
+
+if (!options.input){
+  console.error("Please specify input file")
+  process.exit(1);
+}
+
+
+if (!fs.existsSync(options.input)) {
+  console.error("Cannot find input file");
+  process.exit(1);
+}
+
+const data = fs.readFileSync(options.input, 'utf8')
+const parsed_data = JSON.parse(data);
+
+
+const formatted_result = parsed_data.map(item => {
+  return `${item.exchangedate}:${item.rate}`;
+}).join('\n'); 
+
+if (options.display) {
+  console.log(formatted_result);
+}
+
+if (options.output){
+  fs.writeFileSync(options.output, formatted_result, 'utf8' )
+  console.log(`Results are saved into ${options.output}`);
+}
+
+if (!options.output && !options.display) {
+  process.exit(0);
+}
